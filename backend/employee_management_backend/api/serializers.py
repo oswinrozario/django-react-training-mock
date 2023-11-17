d038bd5795875d1bc3f7ac0d0847c0e83dd628f1
Oswin Rozario <oswinrozario@192.168.1.4>
2023-11-17 13:41:23 +0530

    crud operations basics app_2

--- backend/employee_management_backend/api/serializers.py
+++ backend/employee_management_backend/api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from .models import Student
 
 class StudentSerializer(serializers.Serializer):
     
@@ -6,3 +7,6 @@ class StudentSerializer(serializers.Serializer):
     student_name = serializers.CharField(max_length=100)
     student_roll = serializers.IntegerField()
     student_city = serializers.CharField(max_length=100)
+
+    def create(self, validate_data):
+        return Student.objects.create(**validate_data)
--- backend/employee_management_backend/employee_management_backend/settings.py
+++ backend/employee_management_backend/employee_management_backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'api',
+    'api_2',
 ]
 
 MIDDLEWARE = [
