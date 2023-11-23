60ae7576a13ca706a7b28481ec289395cce9d9b2
Oswin Rozario <oswinrozario@192.168.1.6>
2023-11-23 20:55:19 +0530

    topic - jwt token and simple jwt token framework in django, get/verify/refresh token using simplejwt

--- backend/employee_management_backend/api_4/urls.py
+++ backend/employee_management_backend/api_4/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path,include
+from django.contrib import admin
 from . import views
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView, TokenVerifyView
 
 router = DefaultRouter()
 
@@ -9,6 +11,9 @@ basename='employee')
 
 urlpatterns = [
     path('',include(router.urls)),
+    path('get-token/',TokenObtainPairView.as_view(),name='token_obtain_pair'),
+    path('refresh-token/',TokenRefreshView.as_view(),name='token_refresh'),
+    path('verify-token/',TokenVerifyView.as_view(),name='token_verify'),
 ]
 
  
